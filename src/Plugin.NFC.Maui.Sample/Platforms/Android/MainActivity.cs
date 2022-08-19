14e7e6cc5cd48ec1d2a8b0334844382e65744ee5
Franck BOUR <franckbour@gmail.com>
2022-08-19 13:39:20 +0200

    Fix Android Sample (Maui)
    Add NDEF Discovered intent filter in the MainActivity.cs (MimeType support)

--- src/Plugin.NFC.Maui.Sample/Platforms/Android/MainActivity.cs
+++ src/Plugin.NFC.Maui.Sample/Platforms/Android/MainActivity.cs
@@ -1,11 +1,13 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Nfc;
 using Android.OS;
 
 namespace Plugin.NFC.Maui.Sample
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(new[] { NfcAdapter.ActionNdefDiscovered }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = MainPage.MIME_TYPE)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
